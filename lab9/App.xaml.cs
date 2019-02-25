51fcaa1f67c91b71932784a147e4da758cd0c217
andreas <au561064@post.au.dk>
2019-02-25 16:25:02 +0100

    Setup

--- lab9/App.xaml.cs
+++ lab9/App.xaml.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
-namespace obligatorisk
+namespace AgentAssignment
 {
     /// <summary>
     /// Interaction logic for App.xaml
